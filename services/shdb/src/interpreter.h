c52989bf5daebb3dec80275960c430079322313f
iv4n-t3a <iv4n.t3a@yandex.ru>
2026-03-28 22:17:33 +0300

    Remove extra code

--- services/shdb/src/interpreter.h
+++ services/shdb/src/interpreter.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "aggregate_function.h"
-#include "ast.h"
 #include "db.h"
 #include "rowset.h"
 
@@ -16,15 +15,6 @@ public:
   RowSet Execute(const std::string &query);
 
 private:
-  RowSet ExecuteSelect(const ASTSelectQueryPtr &select_query);
-  void ExecuteInsert(const ASTInsertQueryPtr &insert_query);
-  void ExecuteCreate(const ASTCreateQueryPtr &create_query);
-  void ExecuteDrop(const ASTDropQueryPtr &drop_query);
-
-  void ValidateTable(const std::string &table_name);
-  void ValidateInsertSchema(const std::shared_ptr<Schema> &insert_schema,
-                            const std::shared_ptr<Schema> &table_schema);
-
   std::shared_ptr<Database> db_;
   std::shared_ptr<AggregateFunctionFactory> aggregate_function_factory_;
 };
