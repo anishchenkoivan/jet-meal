a6e43aa4421c33425c88835193840794c302666e
Mikhail Furmanov <furmanov.ma@phystech.edu>
2026-04-19 20:42:00 +0300

    [order] init order service

---
+++ services/order/src/test/java/ru/jetmeal/order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.jetmeal.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
