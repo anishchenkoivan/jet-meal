3bba576a30380b5f681c6f5add1c38e2fde321f1
Your Name <you@example.com>
2026-05-15 23:53:55 +0300

    Add kafka

--- frontend/services/delivery-ssr/next.config.ts
+++ frontend/services/delivery-ssr/next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
   output: "standalone",
   outputFileTracingRoot: path.join(dirname, "../../"),
   transpilePackages: ["@jet-meal/ui-lib"],
-  allowedDevOrigins: ["127.0.0.1", "localhost", "dev.jet.meal", "10.0.2.2"],
+  allowedDevOrigins: ["127.0.0.1", "localhost", "dev.jet-meal.ru", "10.0.2.2"],
   turbopack: {
     root: turbopackRoot,
   },
